92e25c137591839a5ceb0642d9a9887bf20ded35
Carina Rebellon <91596240+rebellon-carina@users.noreply.github.com>
2024-09-28 14:18:41 +0800

    fix another bug

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -15,7 +15,7 @@ else:
 
 
 # Pass the API Key to the OpenAI Client
-client = OpenAI(api_key=OPENAI_KEY))
+client = OpenAI(api_key=OPENAI_KEY)
 
 def get_embedding(input, model='text-embedding-3-small'):
     response = client.embeddings.create(
