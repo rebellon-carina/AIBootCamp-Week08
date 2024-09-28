5f27b7a32ab86fbbb930b193dd43438531f7c09e
Carina Rebellon <91596240+rebellon-carina@users.noreply.github.com>
2024-09-28 14:13:53 +0800

    add OPENAI_KEY

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -6,8 +6,15 @@ import tiktoken
 
 load_dotenv('.env')
 
+if load_dotenv('.env'):
+   # for local development
+   OPENAI_KEY = os.getenv('OPENAI_API_KEY')
+else:
+   OPENAI_KEY = st.secrets['OPENAI_API_KEY']
+
+
 # Pass the API Key to the OpenAI Client
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client = OpenAI(api_key=OPENAI_KEY))
 
 def get_embedding(input, model='text-embedding-3-small'):
     response = client.embeddings.create(
